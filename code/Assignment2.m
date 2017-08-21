4d78846d557294d7eb5f88c3f7383f8ac0046eb7
Torres <yu-zheng15@mails.tsinghua.edu.cn>
2017-08-21 15:42:20 +0800

    Task j finished

--- code/Assignment2.m
+++ code/Assignment2.m
@@ -1,14 +1,21 @@
 clear all;
 close all;
 %% Get the datum, 10 secs case
-sim('LinearWhiteNoise', [0, 10]);
+sim('..\sim\LinearWhiteNoise', [0, 10]);
 N = 1000;                  % # samples
 t10 = u.Time(1:N);
 u10 = u.Data(1:N);
 y10 = y.Data(1:N);
 dt = 0.01;
 whos
-[Cuy10, lag10] = xcov(y10, u10, 'biased');
+[Cuy10_b, lag10] = xcov(y10, u10, 'biased');
+Cyu10_b = xcov(u10, y10, 'biased');
+Cuu10_b = xcov(u10, u10, 'biased');
+Cyy10_b = xcov(y10, y10, 'biased');
+Cuy10_un = xcov(y10, u10, 'unbiased');
+Cyu10_un = xcov(u10, y10, 'unbiased');
+Cuu10_un = xcov(u10, u10, 'unbiased');
+Cyy10_un = xcov(y10, y10, 'unbiased');
 tau10 = lag10 * dt;
 %% Get the datum, 100 secs case
 % Expect a lower variance at higer tau
@@ -29,7 +36,7 @@ figure
 sys1 = tf([1], [0.0025 0.015 1]);
 [y_theo, t_theo] = impulse(sys1);
 plot(t_theo, y_theo, 'r'); hold on;
-plot(tau10, Cuy10/dt, 'g');hold on;
+plot(tau10, Cuy10_b/dt, 'g');hold on;
 plot(tau100, Cuy100/dt, 'b');hold on;
 xlabel('\tau [s]');title('10s & 100s Cross-Covariance fcn & Impulse Function');
 legend({'Theoretical impulse response', '10s Estimation', '100s Estimation'});
@@ -37,3 +44,30 @@ legend({'Theoretical impulse response', '10s Estimation', '100s Estimation'});
 % 1/N; in continuous cases it should be 1/T
 
 
+%% Task2.j Use the command of fftshift to transform auto/cross-covariant
+% functions into auto/cross-spectral densities.
+
+% We choose fftshift&fft rather than fft cuz the center of the sequence has to
+% be shifted.
+N = 1000;                           % 10 secs
+% First : Shift the frequency axis.
+Cuu10_b = fftshift(Cuu10_b);
+Cuy10_b = fftshift(Cuy10_b);
+Cuu10_un = fftshift(Cuu10_un);
+Cuy10_un = fftshift(Cuy10_un);
+% Second : DFT
+f = (1/dt) * (0:.5:N-1) / N;        % N*df=1/dt, The length of the f-axis is
+Suu10_b = fft(Cuu10_b);             % twice as long.
+Suy10_b = fft(Cuy10_b);
+Suu10_un = fft(Cuu10_un);
+Suy10_un = fft(Cuy10_un);
+
+figure;subplot(211);
+loglog(f, abs(Suu10_b), 'r');hold on;
+loglog(f, abs(Suu10_un), 'g');hold on;
+xlabel('f [Hz]');title('Suu');legend({'SuuBiased','SuuUnbiased'});
+
+subplot(212);
+loglog(f, abs(Suy10_b), 'r');hold on;
+loglog(f, abs(Suy10_un), 'g');hold on;
+xlabel('f [Hz]');title('Suy');legend({'SuyBiased','SuyUnbiased'});
